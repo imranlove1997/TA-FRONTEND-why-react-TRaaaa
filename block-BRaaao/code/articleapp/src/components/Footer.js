f2ae446b4acfc864717eb8bbd90aec35f010c818
Imran Khan <imranlove1997@gmail.com>
2022-02-01 08:22:46 +0530

    Article App

---
+++ block-BRaaao/code/articleapp/src/components/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <>
+        <div className="container flex justify-space align-center">
+        <a className="brand">2020 All rights reserved</a>
+        <ul className="flex">
+            <li>Instagram</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+        </ul>
+        </div>
+        </>
+    )
+}
+
+export default Footer;
