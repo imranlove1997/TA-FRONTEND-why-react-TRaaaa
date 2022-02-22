e49efc0a57dcb36bf6c6415763cf3a1b8a37236b
Imran Khan <imranlove1997@gmail.com>
2022-02-22 10:01:11 +0530

    Hydro React

---
+++ block-BRaaav/code/hydro/src/components/App.js
@@ -0,0 +1,23 @@
+import Header from "./Header";
+import Hero from "./Hero";
+import About from "./About";
+import Blog from "./Blog";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+function App() {
+    return (
+        <>
+        <Header />
+        <main>
+        <Hero />
+        <About />
+        <Blog />
+        <Contact />
+        </main>
+        <Footer />
+        </>
+    )
+}
+
+export default App;
